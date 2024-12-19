0fb8ba470fb7a97dcaa5d45748e96d4b9eb3456a
Miracle Mathew <149964081+Knightinmotion@users.noreply.github.com>
2024-12-19 14:40:49 +0100

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -4,5 +4,4 @@ hausa_dict = {"come": "zo",
               "leg": "kafa",
               "food": "adinci"
               }
-print("come go be root possess goat milk cow seed foam later stay animal incests father mother window sparkle "
-      "princess prince bath barf poo food later  ")
+
