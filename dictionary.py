0d84cf562347dd9f21df3e715dc7514a3365468d
Saysmiracle <miraclesayscode@gmail.com>
2024-12-19 14:38:24 +0100

    add your dictionary here

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+hausa_dict = {"come": "zo",
+              "go": "je",
+              "head": "kai",
+              "leg": "kafa",
+              "food": "adinci"
+              }
+print("come go be root possess goat milk cow seed foam later stay animal incests father mother window sparkle "
+      "princess prince bath barf poo food later  ")
